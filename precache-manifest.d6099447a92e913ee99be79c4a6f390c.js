aa8e91abf5f6a42ca06aabd22cc4b525eef6c587
Doriel aboya <doriel494@gmail.com>
2020-09-22 13:01:03 +0300

    Updates

---
+++ precache-manifest.d6099447a92e913ee99be79c4a6f390c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf010a16a923248ce68538c5897405c",
+    "url": "/willing-quick-access/index.html"
+  },
+  {
+    "revision": "a67a5b122e67e21fa173",
+    "url": "/willing-quick-access/static/css/2.9d3b1070.chunk.css"
+  },
+  {
+    "revision": "4175e7d7b8428abf5209",
+    "url": "/willing-quick-access/static/css/main.801784f7.chunk.css"
+  },
+  {
+    "revision": "a67a5b122e67e21fa173",
+    "url": "/willing-quick-access/static/js/2.af2ca98f.chunk.js"
+  },
+  {
+    "revision": "1adb110813a30163ca679a14e0de12c3",
+    "url": "/willing-quick-access/static/js/2.af2ca98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4175e7d7b8428abf5209",
+    "url": "/willing-quick-access/static/js/main.9c250c34.chunk.js"
+  },
+  {
+    "revision": "0bbc0697d7d6db1dd8f2",
+    "url": "/willing-quick-access/static/js/runtime-main.612166f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/willing-quick-access/static/media/logo.5d5d9eef.svg"
+  }
+]);
